2d5884dfd9dbef76b34d109a914b916d08dbd7ca
TomiLevi <126959122+tomil740@users.noreply.github.com>
2024-06-17 12:23:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+function showParagraph() {
+  var a = document.getElementById("myParagraph");
+  if(a.checkVisibility()){
+    a.style.display = "none";
+  }else{
+    a.style.display = "block";
+  }
+}
+
+function hideP(){
+  document.getElementById("myParagraph").style.display = "none"
+  }
