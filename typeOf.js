46578c91b384f31582c0ee3b5cec400b1771e184
tomieedf <tomil@hotmail.co.il>
2024-09-04 16:07:50 +0300

    typeOfExercise

---
+++ typeOf.js
@@ -0,0 +1,28 @@
+const num = 10;
+const str = "Hello";
+const bool = true;
+console.log(`num is of type ${typeof(num)}`);
+console.log(`str is of type ${typeof(str)}`);
+console.log(`bool is of type ${typeof(bool)}`);
+
+const firstVar = 42;
+const secondVar = "42";
+console.log(`is first and sercond var of the same type ${typeof(firstVar)==typeof(secondVar)};`);
+
+let noValue;
+const emptyValue = null;
+console.log(`the type of undefine variable is ${typeof(noValue)}`);
+console.log(`the type of null variable is ${typeof(emptyValue)}`);
+
+const a = 10;
+const b = "1";
+const c = true;
+const d = 10.5;
+const e = 'k';
+
+console.log(`the type of a is ${typeof(a)}`);
+console.log(`the type of b is ${typeof(b)}`);
+console.log(`the type of c is ${typeof(c)}`);
+console.log(`the type of d is ${typeof(d)}`);
+console.log(`the type of e is ${typeof(e)}`);
+
